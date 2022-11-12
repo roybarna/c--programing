4589fc99f7e5d1980934192f051b498c4a1da7c9
roybarna <117443960+roybarna@users.noreply.github.com>
2022-11-12 08:57:11 +0530

    Add files via upload

---
+++ opreatar.c
@@ -0,0 +1,16 @@
+int main() {
+int a, b, c;
+a = 10;
+b = 20;
+
+
+c = a + b;
+printf( "Value of c = %d\n", c);
+c = a - b;
+printf( "Value of c = %d\n", c);
+c = a * b;
+printf( "Value of c = %d\n", c);
+c = b / a;
+printf( "Value of c = %d\n", c);
+c = b % a;
+printf( "Value of c = %d\n",
