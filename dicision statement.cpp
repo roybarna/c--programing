3d0add63be75a9eed0871ca5578e7fe301a7bd0e
roybarna <117443960+roybarna@users.noreply.github.com>
2022-11-22 08:15:51 +0530

    Add files via upload

---
+++ dicision statement.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+int x = 45;
+if( x > 95) {
+printf( "Student is brilliant\n");
+} else {
+printf( "Student is not brilliant\n");
+if(boolean_expression 1) {
+/* Executes when the boolean expression 1 is true */
+}
+else if( boolean_expression 2) {
